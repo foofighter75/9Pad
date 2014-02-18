d7999de4b9353c394c8ee65d6595b49a7248adc3
Benno Vogel <benno.vogel@web.de>
2014-02-18 10:28:18 +0100

    Removed Tiny Webserver

--- start_tiny_webserver.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Starts the TINY web server for testing purposes.
-rem MOVIES WILL NOT WORK WITH THIS WEB SERVER!
-rem Note: TinyWeb is RTLab's tiny WIN32 web server.
-rem http://www.ritlabs.com/en/products/tinyweb/
-tiny "%~dp0"
-
